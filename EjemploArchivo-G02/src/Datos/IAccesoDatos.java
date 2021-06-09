966f6452143d03698ac52f560c43ca7042fc667c
jfseoanesl <35581581+jfseoanesl@users.noreply.github.com>
2021-06-09 10:58:15 -0500

    Add files via upload

---
+++ EjemploArchivo-G02/src/Datos/IAccesoDatos.java
@@ -0,0 +1,17 @@
+
+package Datos;
+
+import Modelo.Motocicleta;
+import java.io.IOException;
+import java.util.List;
+
+
+public interface IAccesoDatos {
+    
+    void registrarMotocicleta(Motocicleta moto) throws IOException;
+    List<Motocicleta> leerMotocicletas()throws IOException;
+    Motocicleta buscarMotocicleta(String placa) throws IOException;
+    List<Motocicleta> consultarMotocicleta(double value) throws IOException;
+    int eliminarMotocicletas(int kms) throws IOException;
+    
+}
